d98bc42fd9e0e341953a7931505091b44de2ac39
root <adamak.tnh@gmail.com>
2025-05-22 13:24:21 +0330

    added start command handler

--- apps/telegram/handlers/commands.py
+++ apps/telegram/handlers/commands.py
@@ -7,7 +7,23 @@ class CommandHandler(BaseHandler):
 
     def __init__(self, update: Update, bot: Telegram):
         super().__init__(update, bot)
+        self.bot = bot
+        self.update = update
 
+    
+    def start_handler(self):
+        self.create_user(step="home")
+        return self.bot.send_message(chat_id=self.chat_id, text="Hello Start Command")
+
+    def help_handler(self):
+        return self.bot.send_message(chat_id=self.chat_id, text="Help Command")
 
     def handle(self):
+
+        if self.update.message.text.startswith("/start"):
+            self.start_handler()
+
+        elif self.update.message.text.startswith("/help"):
+            self.help_handler()
+
         print(f"Command Handlers")
