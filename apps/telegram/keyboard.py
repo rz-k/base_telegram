e0ef6672b5a1d48720be4636b4cc73c8e1fcbbb5
root <adamak.tnh@gmail.com>
2025-05-24 19:04:34 +0330

    add keyboard in project

---
+++ apps/telegram/keyboard.py
@@ -0,0 +1,65 @@
+import json
+
+
+class BaseKeyboard:
+
+    def to_json(self, data: dict):
+        return json.dumps(data)
+
+
+class ReplyKeyboardMarkup(BaseKeyboard):
+
+    def home_keyboard(self):
+        markup = {
+            "keyboard": [
+                ["دکمه اول", "دکمه دوم"],
+                ["دکمه سوم", "دکمه چهارم"]
+            ],
+            "resize_keyboard":True
+        }
+        return self.to_json(
+            data=markup
+        )
+
+    def back_keyboard(self):
+        markup = {
+            "keyboard": [
+                ["بازگشت"]
+            ],
+            "resize_keyboard":True
+        }
+        return self.to_json(
+            data=markup
+        )
+    
+    def remove_keyboard(self):
+        markup = {"keyboard": []}
+        return self.to_json(
+            data=markup
+        )
+
+class InlineKeyboardMarkup(BaseKeyboard):
+
+    def first_keyboard(self):
+        markup = {
+            "inline_keyboard": [
+                [
+                    {"text": "دکمه اول", "callback_data": "first_button"},
+                    {"text": "دکمه دوم", "callback_data": "second_button"}
+                ],
+                [
+                    {"text": "دکمه سوم", "callback_data": "third_button"},
+                    {"text": "دکمه چهارم", "callback_data": "fourth_button"}
+                ]
+            ]
+        }
+        return self.to_json(
+            data=markup
+        )
+
+    def remove_keyboard(self):
+        markup = {"inline_keyboard": []}
+        return self.to_json(
+            data=markup
+        )
+
