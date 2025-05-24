e8138af5cda7637fd07a0d0a7f636a34e4d493ec
root <adamak.tnh@gmail.com>
2025-05-24 16:04:55 +0330

    add example for callback query handler

--- apps/telegram/handlers/callback_queries.py
+++ apps/telegram/handlers/callback_queries.py
@@ -8,5 +8,19 @@ class CallBackQueryHandler(BaseHandler):
     def __init__(self, update: Update, bot: Telegram):
         super().__init__(update, bot)
 
+        self.callback_handlers = {            
+            "check_joined_channel_sponsor": self.handler_joined_channel_sponsor,            
+        }
+
+    def handler_joined_channel_sponsor(self):
+        print("check_joined_channel_sponsor")
+
     def handle(self):
         print(f"CallBackQueryHandler Handlers")
+        
+        callback_data = self.update.callback_query.data or ""
+        base_key = callback_data.split(":", 1)[0] # callback_data is "check_joined_channel_sponsor" or "check_joined_channel_sponsor:user_id"
+
+        handler = self.callback_handlers.get(base_key)
+        if handler:
+            return handler()
