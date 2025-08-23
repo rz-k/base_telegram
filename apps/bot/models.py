c9cd0fc2e40cb75aebc0b623051e27f5b4bf59ce
reza <adamak.tnh@gmail.com>
2025-08-23 20:21:05 +0330

    added update-bot model

--- apps/bot/models.py
+++ apps/bot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class BotUpdateStatus(models.Model):
+    is_update = models.BooleanField(
+        default=False
+    )
+    update_msg = models.TextField(default="bot is updated !")
+
+    def save(self, *args, **kwargs):
+        self.id = 1
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str(f"Bot status is: {self.is_update}")
