a886fb29ed816455d6a8939ad8f6d2811a43fd93
reza <adamak.tnh@gmail.com>
2025-08-24 10:54:49 +0330

    update admin panel model

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.bot.models import BotUpdateStatus, Message
+
+
+@admin.register(BotUpdateStatus)
+class BotUpdateStatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "is_update", "text")
+    list_editable = ("is_update", )
+
+    def text(self, obj):
+        return obj.update_msg[:30]
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "_text")
+    search_fields = ("name", )
+
+    def _text(self, obj):
+        return obj.text[:30]
